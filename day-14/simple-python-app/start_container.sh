11fcd9500c5855a5f13cf1a58e92297afc1fd08c
Amir Shaikh <149932327+Amyshk786@users.noreply.github.com>
2025-09-19 17:27:46 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+echo "docker pull amyshaikh623/simple-python-flask-app:latest"
 
 # Run the Docker image as a container
-echo
+echo "docker run -d -p 5000:5000 amyshaikh623/simple-python-flask-app:latest"
